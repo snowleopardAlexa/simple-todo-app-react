69453e22b57428e17bea8eb3705ea1916919b3bb
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-28 22:24:21 -0500

    fix className

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
        <header>
          <h1>Alexa's Todo List</h1>
+         <Form />
        </header>
     </div>
   );
