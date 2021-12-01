a2c63d9cd6b757a6f7caa4074417b702c143bc9a
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-01 11:44:19 -0500

    add todo id and text

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,7 +5,9 @@ function TodoList({ todos }) {
     return (
          <div className="todo-container">
             <ul className="todo-list">
-               
+               {todos.map(todo => (
+                   <Todo text={todo.text} id={todo.id} />
+               ))}
             </ul>
         </div>
     );
