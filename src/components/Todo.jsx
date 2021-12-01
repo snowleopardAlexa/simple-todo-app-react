614973598348ac833d9bba5a338e1880e1919cc4
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-01 11:33:54 -0500

    add ui btns todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 function Todo() {
     return (
-        <div>
-            
+        <div className="todo">
+            <li className="todo-item">
+                Hello Kion!
+            </li>
+            <button className="complete-btn">
+                <i className="fas fa-check"></i>
+            </button>
+            <button className="trash-btn">
+                <i className="fas fa-trash"></i>
+            </button>
         </div>
     )
 }
