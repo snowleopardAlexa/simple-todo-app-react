f86e8968e3c619799858e1e7ac52cdb770fad54c
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-01 11:29:47 -0500

    add todo component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Todo from "./components/Todo";
 
 function TodoList() {
     return (
          <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                <Todo />
+            </ul>
         </div>
     );
 }
