8ae2daec32170e2bcbd0d551953f2bfd4476461a
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-01 11:41:12 -0500

    pass todos to todolist

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           setTodos={setTodos} 
           setInputText={setInputText} 
         />
-        <TodoList />
+        <TodoList todos={todos} />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Todo from "./components/Todo";
+import Todo from "./Todo";
 
-function TodoList() {
+function TodoList({ todos }) {
     return (
          <div className="todo-container">
             <ul className="todo-list">
-                <Todo />
+               
             </ul>
         </div>
     );
